2b3de519b21e0f3bdc1811780de5d932686f827d
greenteabiscuit <mitani-reishi116@g.ecc.u-tokyo.ac.jp>
2023-12-13 23:17:51 +0900

    Initial Commit

---
+++ enhanced_tokyo_timetable/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  enhanced_tokyo_timetable
+//
+//  Created by lvcha mmm on 2023/12/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ enhanced_tokyo_timetable/enhanced_tokyo_timetableApp.swift
@@ -0,0 +1,17 @@
+//
+//  enhanced_tokyo_timetableApp.swift
+//  enhanced_tokyo_timetable
+//
+//  Created by lvcha mmm on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct enhanced_tokyo_timetableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
